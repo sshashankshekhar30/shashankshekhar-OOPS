0908352ffd1c1482bab0644092105535196f25b2
Shashankshekhar Singh <78536670+sshashankshekhar30@users.noreply.github.com>
2022-06-20 23:54:47 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,37 @@
+package com.greatlearning.models;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	
+	//Constructor
+	public Employee(String firstName, String lastName)
+	{
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+	
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	
+
+}
